9c5cf65c426aba7128a2987bb31d3c74332efab5
Gospinho <54376176+Gospinho@users.noreply.github.com>
2022-04-20 20:18:38 +0200

    Java basics awesoooome training

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Jak masz na imie?");
-        String name = scanner.nextLine();
-        System.out.println("Cześć, " + name);
-        System.out.println("Jak masz na nazwisko?");
-        String lastName = scanner.nextLine();
-        System.out.println("Ile masz lat?");
-        int age = scanner.nextInt();
+//        /*1*/ int x=3;
+//        /*2*/ int y;
+//        /*3*/ System.out.println(x++); //4
+//        /*4*/ System.out.println(++x); // 5
+//        /*5*/ System.out.println(x); // 5
+//        /*6*/ y = x++;
+//        /*7*/ System.out.println (y); // 5
+//        /*8*/ y = ++x;
+//        /*9*/ System.out.println (y); // 7
+//        /*10*/ System.out.println (++y); // 8
 
-        if (age == 18){
-            System.out.println("Właśnie stałeś się pełnoletni!");
-        } else if (age > 18){
-            System.out.println("Wal do monopola!");
-        } else {
-            System.out.println("Szukaj jakiegoś kierownika co Ci kupi");
+
+        int[] tabelaCyfr = new int[5];
+        tabelaCyfr = new int[]{0, 1, 2, 3, 4};
+//        System.out.println(tabelaCyfr[2]);
+        String[] tablicaPieskow = new String[4];
+        tablicaPieskow = new String[]{"Dino", "Kudłaty", "Azorek", "Dyndol"};
+//        System.out.println(tablicaPieskow[2]);
+        // PĘTLA FOR
+
+        for (int i = 0; i < tablicaPieskow.length; i++){
+            System.out.println(tablicaPieskow[i]);
         }
-
-
 
     }
 }
---
+++ src/While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 10) {
+            System.out.println("Próba petla while.");
+            a++;
+        }
+        System.out.println("Koniec pętli.");
+    }
+}
